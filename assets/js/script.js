c4ece18dc624082298391fab6a18e3a63f501de3
LW83 <ljbwilkinson@gmail.com>
2022-05-16 11:40:55 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === 'Enter') {
+            checkAnswer();
+        }
+    })
+    
     runGame("addition");
 })
 
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been prceossed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //Creates to random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -76,11 +85,11 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "-") {
-        return [operand1 + operand2, "subtract"];
+        return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
-        return [operand1 + operand2, "multiply"];
+        return [operand1 * operand2, "multiply"];
     } else if (operator === "/") {
-        return [operand1 + operand2, "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -112,8 +121,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
